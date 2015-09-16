88be063665a963365015b439dfa9c6ac3793521e
fabianhu <fabianwho@gmx.net>
2015-09-16 17:09:56 +0200

    autobuild 16.09.2015 17:09

--- software/fcmbuild.bat
+++ software/fcmbuild.bat
@@ -1,11 +1,13 @@
-@ECHO OFF
+REM @ECHO OFF
+
+set path=%PATH%;C:\Program Files (x86)\Atmel\Atmel Studio 6.2\
 
 REM get date and time
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set fvar=%mydate%%mytime%
 
-git pull > pull.txt
+call git pull origin autobuild > pull.txt
 
 REM get the length of pull.txt
 for %%a in (pull.txt) do (
@@ -15,11 +17,15 @@ set length=%%~za
 if %length% GTR 20 (
 echo building... %fvar% 
 
-set path=%PATH%;"C:\Program Files (x86)\Atmel\Atmel Studio 6.2\"
+
 
 START /WAIT atmelstudio.exe FabOS32.cproj /build debug /out autobuild.txt
 
-REM copy stuff
+@echo on
+call git commit -am "autobuild %fvar%"
+call git push origin autobuild
+@echo off
+
 echo finished... %mytime%
 )
 
